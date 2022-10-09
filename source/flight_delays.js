979cc7331964726cd0f3da56233220467acebb7f
niklauswetter <niklauswetter@arizona.edu>
2022-10-08 19:06:41 -0700

    Final commit

--- source/flight_delays.js
+++ source/flight_delays.js
@@ -8,7 +8,55 @@ var spec = {
             url: "https://raw.githubusercontent.com/picoral/csc-444-data/main/flight_delay.json",
             name: "flight_delays"
         }
-    ]};
+    ],
+    scales: [
+        {
+            name: "xScale",
+            type: "linear",
+            domain: {data: "flight_delays", field: "departureDelay"},
+            range : "width",
+            zero: false
+        },
+        {
+            name: "yScale",
+            type: "linear",
+            domain: {data: "flight_delays", field: "arrivalDelay"},
+            range: "height",
+            zero: false
+        }
+    ],
+    marks: [
+        {
+            type: "symbol",
+            from: {data: "flight_delays"},
+            encode: {
+                enter: {
+                    x: {field: "departureDelay", scale: "xScale"},
+                    y: {field: "arrivalDelay", scale: "yScale"}
+                }
+            }
+        }
+    ],
+    axes: [
+        {
+            scale: "xScale",
+            orient: "bottom",
+            title: "Departure Delay"
+        },
+        {
+            scale: "yScale",
+            orient: "left",
+            title: "Arrival Delay"
+        }
+    ],
+    title: {
+            text: "Flight Arrival Delay vs Departure Delay",
+            fontSize: 20,
+            subtitle: "Values in minutes",
+            subtitleFontSize: 16,
+            align: "left",
+            anchor: "start"
+    }};
 
 // create runtime
 var runtime = vega.parse(spec);
